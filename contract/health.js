0632064fcd9d7902e4aa6e436d9b7561b75a43ba
Seyomico <56334999+Seyomico@users.noreply.github.com>
2020-02-21 05:19:51 +0900

    Fix health.js

--- contract/health.js
+++ contract/health.js
@@ -1,19 +1,32 @@
 class HealthCheck {
-
-    init() {
+    
+    init() { 
     }
 
     /*
-    stringにしてputする
+    配列をstringにしてputする
     k: key
     f: field
     v: value
     */
     _mapPut(k,f,v){
-        const value = JSON.stringify(v)
+        const value = JSON.stringify(v) //const or var?
         storage.mapPut(k, f, value)
     }
 
+    /*
+    stringを配列に戻して返す
+    k: key
+    f: field
+    */
+    _mapGet(k, f) {
+        var v = storage.mapGet(k, f)
+
+        // いなければ空の配列(新規)
+        if (!v) return []
+
+        return JSON.parse(v)
+    }
 
     /*
     配列を取ってくる
@@ -21,11 +34,8 @@ class HealthCheck {
     accountName: アカウント
     */
     _getArray(d, accountName){
-        var userInfo = storage.mapGet(d, accountName)
-
-        // いなければ空の配列(新規)
-        if(!userInfo) return []
-
+        var userInfo = this._mapGet(d, accountName)
+            
         // 配列を返す
         return userInfo
     }
