bb0c7f6ef6e77adf153ea0404bdb0512edd0d9b3
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2020-02-21 08:57:14 +0900

    add index and IOST-SDK-related files

---
+++ ui/src/js/info.js
@@ -0,0 +1,20 @@
+const sk = {
+    // "accountID": "secret key"
+
+}
+const address = {
+    "HealthCheck": "contract address",
+
+}
+const contractInfo = {
+    "HealthCheck": {
+        "address": "replace me",
+        "abi": {
+            "sample": ["replace me", "replace me"],
+        }
+    }
+}
+const config = {
+    // "testnet": "http://13.52.105.102:30001",
+    // "chainId": "replace me", 
+}
